96222c39ea405cd03469d26ab5b081a088a04f62
Pravin Lolage <pravindot17@gmail.com>
2018-12-01 14:42:04 +0530

    correceted typedefitions

--- index.d.ts
+++ index.d.ts
@@ -5,18 +5,18 @@
 declare module 'responsehandler'
 
 export function wrapperCall(func: Function): Promise<any>
-export function handleErrorResponse(err: Any, req: Any, res: Any, next: Function): any
+export function handleErrorResponse(err: any, req: any, res: any, next: Function): any
 export function createResponse(respData: Object, respCode: String): Object
 export function createFailureResponse(err: Error): Object
 
-export function AbstractError(msg, code, errData, functionName, constr): Null
-export function BadRequest(code: String, errData: Object, functionName: String): Null
-export function Unauthorized(code: String, errData: Object, functionName: String): Null
-export function Forbidden(code: String, errData: Object, functionName: String): Null
-export function NotFound(code: String, errData: Object, functionName: String): Null
-export function Unavailable(code: String, errData: Object, functionName: String): Null
-export function UncaughtError(errData: Object, functionName: String): Null
-export function FailedDependency(errData: Object, functionName: String): Null
+export function AbstractError(msg, code, errData, functionName, constr): null
+export function BadRequest(code: String, errData: Object, functionName: String): null
+export function Unauthorized(code: String, errData: Object, functionName: String): null
+export function Forbidden(code: String, errData: Object, functionName: String): null
+export function NotFound(code: String, errData: Object, functionName: String): null
+export function Unavailable(code: String, errData: Object, functionName: String): null
+export function UncaughtError(errData: Object, functionName: String): null
+export function FailedDependency(errData: Object, functionName: String): null
 
-export function setResponseMessages(successMsgs: Object, errorMsgs: Object): Null
+export function setResponseMessages(successMsgs: Object, errorMsgs: Object): null
 
